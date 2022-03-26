e7c8ef7eb02daf62fcb72da7af6b4c760465596c
rafagabidulin <rafagabidulin@gmail.com>
2022-03-26 22:41:04 +0300

    fixed test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,16 +1,15 @@
-import path from 'path';
+import fs from 'fs';
+import path, { dirname } from 'path';
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
-import expectedJSON from '../__fixtures__/expectedJSON.txt';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.__dirname(__filename);
+const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const expectedJSON = readFile('expectedJSON.txt');
 
 test('difference between two JSONs', () => {
-  const filepath1 = getFixturePath('../__fixtures__/file1.json');
-  const filepath2 = getFixturePath('../__fixtures__/file2.json');
-
-  expect(genDiff(filepath1, filepath2)).toBe(expectedJSON);
+  expect(genDiff('file1.json', 'file2.json')).toBe(expectedJSON);
 });
