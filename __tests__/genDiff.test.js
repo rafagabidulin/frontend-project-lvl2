834537fde004a00c4090e9a4e2c8f57f36f9a231
rafagabidulin <rafagabidulin@gmail.com>
2022-03-31 23:03:27 +0300

    added tests for nested files

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -8,20 +8,21 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-const expected = readFile('expected.txt');
+const expectedPlain = readFile('expectedPlain.txt');
+const expectedNested = readFile('expectedNested.txt');
 
 test('difference between two JSONs', () => {
-  expect(genDiff('filepath1.json', 'filepath2.json')).toEqual(expected);
+  expect(genDiff('filepath1.json', 'filepath2.json')).toEqual(expectedPlain);
 });
 
 test('difference between two YAMLs', () => {
-  expect(genDiff('filepath1.yml', 'filepath2.yml')).toEqual(expected);
+  expect(genDiff('filepath1.yml', 'filepath2.yml')).toEqual(expectedPlain);
 });
 
 test('difference between two nested JSONs', () => {
-  expect(genDiff('file1.json', 'file2.json')).toEqual(expected);
+  expect(genDiff('file1.json', 'file2.json')).toEqual(expectedNested);
 });
 
 test('difference between two nested YAMLs', () => {
-  expect(genDiff('file1.yml', 'file2.yml')).toEqual(expected);
+  expect(genDiff('file1.yml', 'file2.yml')).toEqual(expectedNested);
 });
