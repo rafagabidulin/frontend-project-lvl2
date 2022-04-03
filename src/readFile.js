2e8cfe31563c6689e5de6b47463be58339e4f748
rafagabidulin <rafagabidulin@gmail.com>
2022-04-03 21:42:14 +0300

    added readFile.js

---
+++ src/readFile.js
@@ -0,0 +1,12 @@
+import { dirname, resolve } from 'path';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
+
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+export default readFile;
