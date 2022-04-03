cbc825d106b4218dcbdae937e926b24fa76ac507
rafagabidulin <rafagabidulin@gmail.com>
2022-04-03 21:46:57 +0300

    modified stylish

--- src/stylish.js
+++ src/stylish.js
@@ -1,50 +1,58 @@
-import _ from 'lodash';
+const indent = (depth, replacer = ' ', spacesCount = 2) => replacer.repeat(depth * (spacesCount + 2) - 2);
 
-const makeStylish = (data, depth = 1, basicIndent = ' ', spacesAmount = 4) => {
-  const iter = (currentValue, currentDepth) => {
-    if (!_.isObject(currentValue)) {
-      return `${currentValue}`;
-    }
+const stringify = (value, depth = 1) => {
+  if (!(value instanceof Object)) {
+    return String(value);
+  }
 
-    const indentSize = currentDepth * spacesAmount;
-    const currentIndent = basicIndent.repeat(indentSize);
-    const bracketIndent = basicIndent.repeat(indentSize - spacesAmount);
-    const lines = Object
-      .entries(currentValue)
-      .map(([key, val]) => `${currentIndent}${key}: ${iter(val, currentDepth + 1)}`);
+  const keys = Object.keys(value);
+  const result = keys.map((key) => {
+    const prop = value[key];
 
-    return [
-      '{',
-      ...lines,
-      `${bracketIndent}}`,
-    ].join('\n');
-  };
-
-  const indentSize = depth * spacesAmount - 2;
-  const currentIndent = basicIndent.repeat(indentSize);
-  const bracketIndent = basicIndent.repeat(indentSize - 2);
-
-  const output = data.flatMap(({
-    key, status, value, valueBefore, valueAfter, children,
-  }) => {
-    switch (status) {
-      case 'nested':
-        return `${currentIndent}  ${key}: ${makeStylish(children, depth + 1)}`;
-      case 'changed':
-        return `${currentIndent}- ${key}: ${iter(valueBefore, depth + 1)}\n${currentIndent}+ ${key}: ${iter(valueAfter, depth + 1)}`;
-      case 'added':
-        return `${currentIndent}+ ${key}: ${iter(value, depth + 1)}`;
-      case 'removed':
-        return `${currentIndent}- ${key}: ${iter(value, depth + 1)}`;
-      default:
-        return `${currentIndent}  ${key}: ${iter(value, depth + 1)}`;
-    }
+    return `${indent(depth + 1)}  ${key}: ${stringify(prop, depth + 1)}`;
   });
-  return [
-    '{',
-    ...output,
-    `${bracketIndent}}`,
-  ].join('\n');
+
+  return `{\n${result.join('\n')}\n  ${indent(depth)}}`;
 };
 
-export default makeStylish;
+const stylish = (nodes) => {
+  const iter = (node, depth = 0) => {
+    const {
+      key,
+      type,
+      value,
+      children,
+      removedValue,
+      addedValue,
+    } = node;
+
+    switch (type) {
+      case 'root': {
+        return `${children.map((child) => iter(child, depth + 1)).join('')}`;
+      }
+      case 'nested': {
+        return `\n${indent(depth)}  ${key}: {${children.map((child) => iter(child, depth + 1)).join('')}\n${indent(depth)}  }`;
+      }
+      case 'deleted': {
+        return `\n${indent(depth)}- ${key}: ${stringify(value, depth)}`;
+      }
+      case 'added': {
+        return `\n${indent(depth)}+ ${key}: ${stringify(value, depth)}`;
+      }
+      case 'changed': {
+        const removed = `\n${indent(depth)}- ${key}: ${stringify(removedValue, depth)}`;
+        const added = `\n${indent(depth)}+ ${key}: ${stringify(addedValue, depth)}`;
+        return `${removed}${added}`;
+      }
+      case 'unchanged': {
+        return `\n${indent(depth)}  ${key}: ${stringify(value, depth)}`;
+      }
+      default:
+        return null;
+    }
+  };
+
+  return `{${iter(nodes)}\n}`;
+};
+
+export default stylish;
