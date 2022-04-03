8cb1827f122a6e5276ac5bb0ea3eeb107527802e
rafagabidulin <rafagabidulin@gmail.com>
2022-04-03 21:41:18 +0300

    fixed format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,8 +10,8 @@ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2, format) => {
-    console.log(genDiff(filepath1, filepath2, format));
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
 
 program.parse();
