66a242be42bf2000764594866e5995ef769f91df
rafagabidulin <rafagabidulin@gmail.com>
2022-04-04 22:32:56 +0300

    added formatter plain

---
+++ src/formatters/plain.js
@@ -0,0 +1,42 @@
+const getValue = (value) => {
+  if (value instanceof Object) {
+    return '[complex value]';
+  }
+  return typeof (value) === 'string' ? `${value}` : value;
+};
+
+const plain = (nodes) => {
+  const iter = (node, acc = '') => {
+    const {
+      key,
+      type,
+      value,
+      children,
+      removedValue,
+      addedValue,
+    } = node;
+
+    switch (type) {
+      case 'root': {
+        return children.map((child) => iter(child, key)).join('');
+      }
+      case 'nested': {
+        return children.map((child) => iter(child, `${acc}${key}.`)).join('');
+      }
+      case 'removed': {
+        return `Property ${acc}${key} was removed`;
+      }
+      case 'added': {
+        return `Property ${acc}${key} was added with value: ${getValue(value)}`;
+      }
+      case 'updated': {
+        return `Property ${acc}${key} was updated. From ${removedValue} to ${getValue(addedValue)}`;
+      }
+      default:
+        return null;
+    }
+  };
+  return iter(nodes).filter((child) => child !== null).join('/n');
+};
+
+export default plain;
