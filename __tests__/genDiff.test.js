6d481ab9787a769874ce216c610ff273271320d9
rafagabidulin <rafagabidulin@gmail.com>
2022-04-04 21:32:58 +0300

    renamed expectedPlain to expectedOutput

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -8,15 +8,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-const expectedPlain = readFile('expectedPlain.txt');
+const expectedOutput = readFile('expectedOutput.txt');
 const expectedNested = readFile('expectedNested.txt');
 
 test('difference between two JSONs', () => {
-  expect(genDiff('filepath1.json', 'filepath2.json')).toEqual(expectedPlain);
+  expect(genDiff('filepath1.json', 'filepath2.json')).toEqual(expectedOutput);
 });
 
 test('difference between two YAMLs', () => {
-  expect(genDiff('filepath1.yml', 'filepath2.yml')).toEqual(expectedPlain);
+  expect(genDiff('filepath1.yml', 'filepath2.yml')).toEqual(expectedOutput);
 });
 
 test('difference between two nested JSONs', () => {
