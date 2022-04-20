5b159bb078c3da8da62ee77731499e27ee600cb4
rafagabidulin <rafagabidulin@gmail.com>
2022-04-20 21:56:47 +0300

    renamed diff.js to index.js

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path, { dirname } from 'path';
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
-import genDiff from '../src/diff.js';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
